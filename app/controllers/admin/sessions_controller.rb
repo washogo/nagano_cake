4d21e1a09ec47d4fb219b90de16f2a0ca3d0eff3
EC2 Default User <ec2-user@ip-172-31-46-236.ec2.internal>
2021-10-09 08:33:42 +0000

    [Add]admin_ログイン画面_controller_sessions

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -4,9 +4,18 @@ class Admin::SessionsController < ApplicationController
   end
   
   def create
+    admin=Admin.find_by(email: params[:session][:email].downcase)
+    if admin && admin.authenticate(params[:session][:password])
+      session[:admin_email]=admin.email
+      redirect_to "/admins/items"
+    else
+      render :new
+    end
   end
   
   def destroy
+    session[:admin_email]=nil
+    redirect_to new_admin_session_path
   end
   
 end
