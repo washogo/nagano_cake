0e52947fb1d3ef5389065d3f7e9382b5f2845f4c
EC2 Default User <ec2-user@ip-172-31-46-236.ec2.internal>
2021-10-11 01:53:06 +0000

    [Add]admin_ジャンル一覧・追加画面のcreateアクションとストロングパラメータを設定

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,14 +1,26 @@
 class Admin::GenresController < ApplicationController
+  @genre=Genre.new
+  
   def index
     @genres=Genre.all
   end
   
   def create
+    @genre= Genre.new(genre_params)
+    @genre.save
+    redirect_to admin_genres_path
   end
   
   def edit
+    
   end
   
   def update
+  end
+  
+  private
+
+  def genre_params
+    params.permit(:name)
   end
 end
