d549c243a4803afa12226ae7bdb4aee69cf60718
EC2 Default User <ec2-user@ip-172-31-46-236.ec2.internal>
2021-10-13 09:10:03 +0000

    [Add]画像のRuntimeErrorでsecretキーを保存,[Add]商品詳細画面に画像を表示

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -12,7 +12,9 @@ class Admin::ItemsController < ApplicationController
   def index
     @items=Item.page(params[:page])
   end
+  
   def show
+    @item=Item.find(params[:id])
   end 
   
   def edit
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '137dbdf965e56f0a22e1f2cf7eb7ee5f647a4f5e2208be3eeff189aef6df5b3e88e678693f38d3e48b709acadff985faeb678f73f0a42c4dda925a503853dafa'
