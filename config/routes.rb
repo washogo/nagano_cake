bcd64529a59cf027b8ba63bb72c31950fcb47188
EC2 Default User <ec2-user@ip-172-31-46-236.ec2.internal>
2021-10-14 15:15:23 +0000

    [Start]public_トップページ

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,10 @@ Rails.application.routes.draw do
     resources :items,except:[:destroy]
   end
   
+  root to: 'homes#top'
+  get '/about',to:'homes#about'
+ 
+  namespace :public do
+  end 
+  
 end
