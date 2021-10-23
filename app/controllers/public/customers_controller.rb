c86b93491c820580da5a24e8f9b35c7047d8f545
washogo <aiz4338150@gmail.com>
2021-10-23 06:21:37 +0000

    [Fix]public_会員情報編集画面_updateアクションとストロングパラメータ

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -9,6 +9,9 @@ before_action :authenticate_customer!
   end
   
   def update
+    @customer=current_customer
+    @customer.update(customer_params)
+    redirect_to current_customer_path
   end
   
   def cancel
@@ -17,4 +20,8 @@ before_action :authenticate_customer!
   def quit
   end
   
+  private
+  def customer_params
+    params.require(:customer).permit(:last_name,:first_name,:last_name_kana,:first_name_kana,:postal_code,:address,:telephone_number,:email)
+  end
 end
