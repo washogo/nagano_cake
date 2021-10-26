5fd415046782e7ef752a294e6920195c6cb1a4a6
washogo <aiz4338150@gmail.com>
2021-10-26 03:53:10 +0000

    [Fix]public_配送先一覧画面

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -17,6 +17,7 @@ class Public::AddressesController < ApplicationController
   
   def update
     @address=Address.find(params[:id])
+    
     @address.update(address_params)
     redirect_to addresses_path
   end
--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,2 +1,20 @@
 class Public::CartItemsController < ApplicationController
+  def index
+    @cart_item=CartItem.new
+    @cart_items=CartItem.all
+  end
+  
+  def create
+    @cart_item=CartItem.new
+    @cart_item.item_id=Item.id
+    @cart_item.customer_id=current_customer.id
+    @cart_item.save
+    redirect_to cart_items_path
+  end
+  
+  def update
+    @cart_item=CartItem.find(params[:id])
+    
+  end
+  
 end
