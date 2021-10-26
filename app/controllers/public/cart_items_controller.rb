5fe9d05d2257861dbe39ade755b7a343bb9566a6
washogo <aiz4338150@gmail.com>
2021-10-26 03:58:53 +0000

    [Add]public_cart_itemsコントローラの作成

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -14,7 +14,28 @@ class Public::CartItemsController < ApplicationController
   
   def update
     @cart_item=CartItem.find(params[:id])
-    
+    @cart_item.item_id=Item.id
+    @cart_item.customer_id=current_customer.id
+    @cart_item.update(cart_item_params)
+    redirect_to cart_items_path
+  end
+  
+  def dstroy
+    @cart_item=CartItem.find(params[:id])
+    @cart_item.dstroy
+    redirect_to cart_items_path
+  end
+  
+  def destroy_all
+    @cart_items=CartItem.all
+    @cart_items.destroy
+    redirect_to cart_items_path
+  end
+  
+  private
+  
+  def cart_item_params
+    params.require(:cart_item).permit(:amount)
   end
   
 end
