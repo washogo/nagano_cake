1bd00ec74505b162bef509c5ece4ccc24c2e9e6c
washogo <aiz4338150@gmail.com>
2021-10-30 03:43:32 +0000

    [Add]public_注文情報入力画面_アクションの定義とルーティングの設定

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,2 +1,21 @@
 class Public::OrdersController < ApplicationController
+  def new
+    @order=Order.new
+  end
+  
+  def create
+  end
+  
+  def index
+  end
+  
+  def show
+  end
+  
+  def confirmation
+  end
+  
+  def completed
+  end
+  
 end
